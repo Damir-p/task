2e7c31fc82052f73747ca089369b361f7dd6d638
Damir <damirbek.ptn@gmail.com>
2023-02-08 14:15:17 +0600

    2- commit

--- task/models.py
+++ task/models.py
@@ -3,25 +3,33 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    id = models.IntegerField(_(""))
-    name = models.IntegerField(_(""))
-    books = models.CharField(_(""), max_length=50)
+    id = models.IntegerField("ID", unique=True, primary_key=True)
+    name = models.IntegerField("имя", default=0)
+    books = models.CharField("Книги", max_length=50)
     
+    def __str__(self):
+        return self.name
     
 class Category(models.Model):
-    id = models.IntegerField(_(""))
-    name = models.CharField(_(""), max_length=50)
+    id = models.IntegerField("ID", unique=True, primary_key=True)
+    name = models.CharField("имя", max_length=50)
     
-    
+    def __str__(self):
+        return self.name
     
 class Tag(models.Model):
-    id = models.IntegerField(_(""))
-    name = models.CharField(_(""), max_length=50)
+    id = models.IntegerField("ID", unique=True, primary_key=True)
+    name = models.CharField("имя", max_length=50)
     
+    def __str__(self):
+        return self.name
     
 class Book(models.Model):
-    id = models.IntegerField(_(""))
-    name = models.CharField(_(""), max_length=50)
-    category_id = models.CharField("")
-    author_id = models.CharField("")
-    tags = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    id = models.IntegerField("ID", unique=True, primary_key=True)
+    name = models.CharField("имя", max_length=50)
+    category_id = models.IntegerField("Категории ID", default=0)
+    author_id = models.IntegerField("Автор книг", default=0)
+    tags = models.ManyToManyField("Tag", verbose_name=("Теги"))
+    
+    def __str__(self):
+        return self.name
